0240663a183f2fdeded7161a06a2ca41df6847dc
Bill Hammond <willmhammond@gmail.com>
2019-07-13 21:48:22 -0700

    fixed logic for Pyschic-Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,56 @@
 //array of the alphabet for computer to choose
-var compChoices = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m",];
+var letters = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m", ];
 
 //var to track wins, losses, and guesses
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var lettersGuessed = [];
-
+var lettersGuessed = 0;
+var letterArray = [];
+var randomGen = randomGen;
 //computers random letter choice
-var randomGen = Math.floor(Math.random() * alphabet.length);
-var compChoice = alphabet[randomGen];
+randomGen = letters[Math.floor(Math.random() * letters.length)];
+console.log(randomGen);
+//var compChoice = letters[randomGen];
 
-console.log(compChoice);
+function compChoice() {
+	randomGen = letters[Math.floor(Math.random() * letters.length)];
+	console.log(randomGen);
+}
 
+document.onkeyup = function (event) {
+
+		var userGuess = event.key;
+
+		if (userGuess === randomGen) {
+			wins++;
+			guessesLeft = 9;
+			letterArray = [];
+		}
+
+		compChoice();
+		if (guessesLeft == 0) {
+			losses++;
+			guessesLeft = 9;
+			letterArray = [];
+
+		} else if (/[a-zA-Z]/.test(userGuess)) {
+			guessesLeft--;
+			letterArray.push(userGuess);
+		}
+		document.getElementById("wins").textContent = "Wins: " + wins;
+		document.getElementById("losses").textContent = "Losses: " + losses;
+		document.getElementById("guessesLeft").textContent = "Guesses left: " + guessesLeft;
+		document.getElementById("lettersGuessed").textContent = "Guesses so far: " + letterArray;
+
+	}
+		/*function checkGuess(theKey) {
+			if (theKey === compChoice) {
+				alert("You are correct!")
+				wins++;
+			} else {
+				alert("Try again!");
+				guessesLeft--;
+			}
+
+		}*/
